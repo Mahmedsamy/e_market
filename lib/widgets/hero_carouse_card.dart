183cddf70023ca1aaedf69750046a91ffb7beead
padmin1 <mdiaa203@gmail.com>
2023-06-13 17:05:49 +0200

    13/6/2023

---
+++ lib/widgets/hero_carouse_card.dart
@@ -0,0 +1,48 @@
+import 'package:e_market/models/category_model.dart';
+import 'package:flutter/material.dart';
+
+class HeroCarouseCard extends StatelessWidget {
+  final Category category;
+
+  const HeroCarouseCard({Key? key, required this.category}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(5.0),
+      child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+          child: Stack(
+            children: <Widget>[
+              Image.network(category.imageUrl!,
+                  fit: BoxFit.cover, width: 1000.0),
+              Positioned(
+                bottom: 0.0,
+                left: 0.0,
+                right: 0.0,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(200, 0, 0, 0),
+                        Color.fromARGB(0, 0, 0, 0)
+                      ],
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                    ),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 20.0),
+                  child: Text(category.name!,
+                      style: Theme.of(context)
+                          .textTheme
+                          .displayLarge!.copyWith(color: Colors.white)
+
+                  ),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
